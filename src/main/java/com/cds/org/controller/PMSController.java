34d19e9092211daaa2efd855f4a5615ffd12ac83
Chandrakant <chandrakant@ChandrakantsMBP.lan>
2022-04-14 14:37:24 -0400

    Reafactoring PMSController

--- src/main/java/com/cds/org/controller/PMSController.java
+++ src/main/java/com/cds/org/controller/PMSController.java
@@ -6,7 +6,16 @@ import com.cds.org.mapper.PMSMapper;
 import com.cds.org.model.ClientDetails;
 import com.cds.org.service.PMSService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,16 +28,26 @@ public class PMSController {
     private PMSMapper mapper;
 
     @PostMapping("/client")
-    public void addPMSClient(@RequestBody ClientDetailsDTO dto)
+    public ResponseEntity<ClientDetailsDTO> addPMSClient(@RequestBody ClientDetailsDTO dto)
     {
         ClientDetails clientDetails = mapper.clientDetailsDTOToEntity(dto);
         service.addClient(clientDetails);
+        ClientDetailsDTO clientDetailsDTO = mapper.clientDetailsEntityToDto(clientDetails);
+        return new ResponseEntity<>(clientDetailsDTO,HttpStatus.CREATED);
     }
 
 
     @GetMapping("/client")
-    public Iterable<ClientDetails> getAllClients()
+    public ResponseEntity<List<ClientDetailsDTO>> getAllClients()
     {
-       return  service.getAllClients();
+        Iterable<ClientDetails> allClients = service.getAllClients();
+        List<ClientDetails> clientDetails = StreamSupport.stream(allClients.spliterator(), false)
+                .collect(Collectors.toList());
+
+        List<ClientDetailsDTO> dtoList = new ArrayList<>();
+        for (ClientDetails clientDetail : clientDetails) {
+            dtoList.add(mapper.clientDetailsEntityToDto(clientDetail));
+        }
+        return new ResponseEntity(dtoList,HttpStatus.OK);
     }
 }
